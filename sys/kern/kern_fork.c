38377a286fdf0318574de7271cc87c19e36c90c3
Oliver Pinter <oliver.pntr@gmail.com>
2013-11-01 02:12:55 +0100

    OPBSD: fork - when maxproc limit exceeded print td_name too
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>
    (cherry picked from commit 971b9864179448a40f81b6029832f87b8cb3c7d7)

--- sys/kern/kern_fork.c
+++ sys/kern/kern_fork.c
@@ -859,9 +859,9 @@ fork1(struct thread *td, int flags, int pages, struct proc **procp,
 	    PRIV_MAXPROC, 0) != 0) || nprocs_new >= maxproc) {
 		sx_xlock(&allproc_lock);
 		if (ppsratecheck(&lastfail, &curfail, 1)) {
-			printf("maxproc limit exceeded by uid %u (pid %d); "
+			printf("maxproc limit exceeded by uid %u (pid %d td_name %s); "
 			    "see tuning(7) and login.conf(5)\n",
-			    td->td_ucred->cr_ruid, p1->p_pid);
+			    td->td_ucred->cr_ruid, p1->p_pid, td->td_name);
 		}
 		sx_xunlock(&allproc_lock);
 		error = EAGAIN;
